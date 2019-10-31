77b6efb36f23f8962800de077b12e38efc8f2c3a
Andreas Ferstad <aoferstad@gmail.com>
2019-10-31 11:18:16 +0100

    Run KNN uncertainty experiment and recorded results

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ census, census_with_dummies = get_census_data(adult_data_url, adult_test_url)
 experiments = []
 results = pd.DataFrame(columns=['initial_accuracy', 'final_accuracy'])
 
-for i in range(2):
+for i in range(5):
     print(i)
-    experiments.append(Experiment(census_with_dummies, seed=i, n_points_to_add_at_a_time = 500))
+    experiments.append(Experiment(census_with_dummies, seed=i, n_points_to_add_at_a_time = 1))
     experiments[i].run_experiment(method='uncertainty')
 
     results.loc[i] = [experiments[i].model_initial_accuracy, experiments[i].model_final_accuracy]
