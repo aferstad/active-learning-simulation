7d8a6d20d1001fcae18381d2ff270c94a8614ef0
Andreas Ferstad <aoferstad@gmail.com>
2020-03-12 16:17:23 +0100

    add default 300 component pca to voice data

--- input/voice/voice_import.py
+++ input/voice/voice_import.py
@@ -1,9 +1,11 @@
 # https://archive.ics.uci.edu/ml/datasets/ISOLET
 
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
-def get_voice_data():
+def __get_raw_voice_data():
     # IMPORT DATA AND MAKE FIRST COLUMN LABEL
     data_train = pd.read_csv('input/voice/isolet1+2+3+4.data', header=None)
     data_test = pd.read_csv('input/voice/isolet5.data', header=None)
@@ -12,3 +14,28 @@ def get_voice_data():
     data = data.drop(columns = [617])
     data.insert(0, 'label', data_label)
     return data
+
+
+def get_voice_data(n_components = 300):
+    data = __get_raw_voice_data()
+
+    scaler = StandardScaler()
+    pca = PCA(n_components=n_components)
+
+    X = data.iloc[:,1:]
+    y = data.iloc[:,0]
+
+    X = scaler.fit(X)
+    X = scaler.transform(X)
+
+    X = pca.fit(X)
+    X = pca.transform(X)
+
+    pct_variance_kept = sum(pca.explained_variance_ratio_[:n_components])
+
+    print('variance kept due to PCA: ' + str(round(pct_variance_kept, 2)))
+
+    data_pca = pd.DataFrame(X)
+    data_pca.insert(0, 'label', y)
+
+    return data_pca
